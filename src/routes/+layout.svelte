b4a7505330f03a76745a41fff8c109a0cbfec621
Jesus-Cries <16zeichen@gmail.com>
2022-11-22 00:10:36 +0100

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
     import "../app.css";
+    import Footer from "./Footer.svelte";
 </script>
 
 <div class=""><slot /></div>
+<Footer />
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,1 @@
+<div class="p-4 text-center text-white text-sm bg-[#222222]">Copyright © 2022 Leon Obermann</div>
