5da190ea7fc9768fce5d40932e40bb7bd48809ee
Jesus-Cries <16zeichen@gmail.com>
2022-11-24 00:49:46 +0100

    Create on load and on view transition components

---
+++ src/routes/OnLoadTransition.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let play: boolean = false;
+    onMount(() => {
+        play = true;
+    });
+</script>
+
+{#if play}
+    <slot />
+{:else}
+    <div class="w-full opacity-0">
+        <slot />
+    </div>
+{/if}
---
+++ src/routes/OnViewTransition.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { inview } from "svelte-inview";
+
+    export let repeat: boolean = false;
+
+    let isInView: boolean = false;
+</script>
+
+<div
+    use:inview={{ unobserveOnEnter: !repeat, rootMargin: "-10%" }}
+    on:change={({ detail }) => {
+        isInView = detail.inView;
+    }}
+>
+    {#if isInView}
+        <slot />
+    {:else}
+        <div class="w-full opacity-0">
+            <slot />
+        </div>
+    {/if}
+</div>
