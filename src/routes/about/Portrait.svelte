b3d3533adb72f3a264a1fc1856eb578cd55302ee
Jesus-Cries <16zeichen@gmail.com>
2022-11-28 00:34:13 +0100

    WIP: Fix displacement of content due to portrait animation

--- src/routes/about/Portrait.svelte
+++ src/routes/about/Portrait.svelte
@@ -1,32 +1,44 @@
 <script lang="ts">
-    import { fly } from "svelte/transition";
+    import { fly, scale } from "svelte/transition";
 
     import OnLoadTransition from "../OnLoadTransition.svelte";
+
+    let width: number = 0;
+    let divHeight: number = 0;
+    $: divHeight = (width / 2 + width / 30) / 3;
+
+    $: console.log(divHeight >= 426.66 ? 426.66 : divHeight);
     // TODO: Find out how to fix displacement during animation
 </script>
 
-<!-- <OnLoadTransition> -->
-<div
-    class="w-full relative mt-12 flex flex-col justify-center items-center content-center"
-    in:fly={{ y: 200, duration: 1500 }}
->
-    <!-- FIXME: Small hexagons don't stick to positions -->
-    <div class="w-2/5 max-w-xs relative">
-        <div class="w-3 h-3 absolute bottom-10 left-1/3 bg-primary hex" />
-        <div class="w-5 h-5 absolute bottom-2 left-1/2 bg-primary hex" />
-        <div class="w-7 h-7 absolute top-0 right-3/4 bg-primary hex" />
-        <div class="w-9 h-9 absolute top-0 left-3/4 bg-primary hex" />
+<svelte:window bind:outerWidth={width} />
+<OnLoadTransition>
+    <div class="container w-full max-h-[426.66px]" style="--container-height: {divHeight}">
+        <div
+            class="w-full relative mt-12 flex flex-col justify-center items-center content-center"
+            in:fly={{ y: -200, duration: 1250 }}
+        >
+            <!-- FIXME: Small hexagons don't stick to positions -->
+            <div class="w-2/5 max-w-xs relative">
+                <div class="w-3 h-3 absolute bottom-10 left-1/3 bg-primary hex" />
+                <div class="w-5 h-5 absolute bottom-2 left-1/2 bg-primary hex" />
+                <div class="w-7 h-7 absolute top-0 right-3/4 bg-primary hex" />
+                <div class="w-9 h-9 absolute top-0 left-3/4 bg-primary hex" />
+            </div>
+            <div class="w-2/5 max-w-xs h-full absolute bg-primary bg-hex" />
+            <img
+                src="Portrait-min-small-min.png"
+                alt="Portrait of Leon Obermann"
+                class="w-2/5 max-w-xs image-hex"
+            />
+        </div>
     </div>
-    <div class="w-2/5 max-w-xs h-full absolute bg-primary bg-hex" />
-    <img
-        src="Portrait-min-small-min.png"
-        alt="Portrait of Leon Obermann"
-        class="w-2/5 max-w-xs image-hex"
-    />
-</div>
+</OnLoadTransition>
 
-<!-- </OnLoadTransition> -->
 <style>
+    .container {
+        height: var(--container-height) px;
+    }
     .image-hex {
         clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
     }
