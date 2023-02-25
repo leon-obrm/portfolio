fc6c8e942c48b6c0f29bd13fa235e4e2d8cb2197
Jesus-Cries <16zeichen@gmail.com>
2023-02-25 21:02:18 +0100

    Refactor layout of headline

--- src/routes/about/Headline.svelte
+++ src/routes/about/Headline.svelte
@@ -1,16 +1,12 @@
-<div
-    class="w-3/4 flex flex-col justify-center content-start items-center gap-2 md:w-full md:max-w-lg "
->
-    <div class="flex flex-col justify-center items-start content-center md:w-full">
-        <h1 class="text-3xl font-bold md:text-4xl lg:text-5xl uppercase">Hey, I am</h1>
-        <h1 class="text-3xl font-bold md:text-4xl lg:text-5xl uppercase">Leon Obermann</h1>
-    </div>
-    <div class="flex flex-col justify-center items-start content-center md:w-full">
-        <h1 class="text-2xl font-semibold text-primary md:text-3xl lg:text-4xl uppercase">
-            A software designer
-        </h1>
-        <h1 class="text-2xl font-semibold text-primary md:text-3xl lg:text-4xl uppercase">
-            based in Düsseldorf
-        </h1>
+<div class="w-3/4 flex justify-center md:justify-start md:w-full md:max-w-lg uppercase">
+    <div>
+        <div class="text-3xl font-bold md:text-4xl lg:text-5xl">
+            <h1>Hey, I am</h1>
+            <h1>Leon Obermann</h1>
+        </div>
+        <div class="text-2xl font-semibold text-primary md:text-3xl lg:text-4xl mt-2">
+            <h1>A software designer</h1>
+            <h1>based in Düsseldorf</h1>
+        </div>
     </div>
 </div>
