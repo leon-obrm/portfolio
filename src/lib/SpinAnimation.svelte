c4026e9ded870dfc0b60fac1f84ea34ed8021c8e
Jesus-Cries <16zeichen@gmail.com>
2023-02-25 19:50:38 +0100

    Add spin animation to portrait

---
+++ src/lib/SpinAnimation.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { Motion } from "svelte-motion";
+
+    export let delay: number = 0;
+    export let className: string = "";
+</script>
+
+<Motion
+    let:motion
+    initial={{ rotate: 180, scale: 0 }}
+    animate={{ rotate: 360, scale: 1 }}
+    transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+        delay,
+    }}
+    whileHover={{ scale: 1.05 }}
+>
+    <div use:motion class={className}>
+        <slot />
+    </div>
+</Motion>
+
+<style>
+    .hex {
+        clip-path: polygon(50% 0%, 95% 25%, 95% 75%, 50% 100%, 5% 75%, 5% 25%);
+    }
+</style>
--- src/routes/about/Portrait.svelte
+++ src/routes/about/Portrait.svelte
@@ -5,29 +5,29 @@
 -->
 <script lang="ts">
     import { scale } from "svelte/transition";
+    import { Motion } from "svelte-motion";
 
     import OnLoadTransition from "../../lib/OnLoadTransition.svelte";
+    import SpinAnimation from "../../lib/SpinAnimation.svelte";
 </script>
 
-<OnLoadTransition>
-    <div
-        class="w-full relative mt-12 flex flex-col justify-center items-center content-center"
-        in:scale={{ duration: 500 }}
-    >
-        <div class="w-2/5 max-w-xs relative md:w-4/5">
-            <div class="w-3 h-3 absolute bottom-10 left-1/3 bg-primary hex" />
-            <div class="w-5 h-5 absolute bottom-2 left-1/2 bg-primary hex" />
-            <div class="w-7 h-7 absolute top-0 right-3/4 bg-primary hex" />
-            <div class="w-9 h-9 absolute top-0 left-3/4 bg-primary hex" />
-        </div>
+<div class="w-full relative mt-12 flex flex-col justify-center items-center content-center">
+    <div class="w-2/5 max-w-xs relative md:w-4/5">
+        <SpinAnimation delay={0.3} className="w-3 h-3 absolute bottom-10 left-1/3 bg-primary hex" />
+        <SpinAnimation delay={0.2} className="w-5 h-5 absolute bottom-2 left-1/2 bg-primary hex" />
+        <SpinAnimation delay={0.2} className="w-7 h-7 absolute top-0 right-3/4 bg-primary hex" />
+        <SpinAnimation delay={0.1} className="w-9 h-9 absolute top-0 left-3/4 bg-primary hex" />
+    </div>
+
+    <SpinAnimation className="w-full relative flex flex-col items-center">
         <div class="w-2/5 max-w-xs h-full absolute bg-primary bg-hex md:w-4/5" />
         <img
             src="Portrait-min-small-min.png"
             alt="Portrait of Leon Obermann"
             class="w-2/5 max-w-xs image-hex md:w-4/5"
         />
-    </div>
-</OnLoadTransition>
+    </SpinAnimation>
+</div>
 
 <style>
     .image-hex {
