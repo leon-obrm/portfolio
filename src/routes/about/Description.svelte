2a39608e5f51bdac0566ca860caf5c8ab6360cc7
Jesus-Cries <16zeichen@gmail.com>
2023-06-16 18:46:08 +0200

    Open university link in new tab

--- src/routes/about/Description.svelte
+++ src/routes/about/Description.svelte
@@ -1,7 +1,11 @@
 <div class="w-3/4 max-w-md flex flex-col justify-center content-start gap-5 md:w-full md:max-w-lg">
     <p class="w-full text-justify text-sm md:text-md lg:text-lg">
         I graduated with a bachelor’s degree in computer science from the
-        <a class="link text-primary-700" href="https://www.dhbw-stuttgart.de/en/"
+        <a
+            class="link text-primary-700"
+            href="https://www.dhbw-stuttgart.de/en/"
+            target="_blank"
+            rel="noopener noreferrer"
             >DHBW Stuttgart
         </a>
         in 2022. In the process, I learned a lot about algorithms and code design and discovered my passion
