f46a875161f1aea95c27872ef80d1b01fa05432c
Jesus-Cries <16zeichen@gmail.com>
2023-09-14 19:31:36 +0200

    Show actual hue gradient on hue slider

--- src/routes/projects/colors/HueSlider.svelte
+++ src/routes/projects/colors/HueSlider.svelte
@@ -14,13 +14,58 @@
 
 <div class="flex w-full max-w-md flex-col gap-2">
     <label for="hue-rotation-amount" class="font-semibold">Tint</label>
-    <!-- TODO: Replace with gradient of actual hues  -->
     <input
+        class="slider h-3 w-full rounded-full"
+        style="background: linear-gradient(90deg, hsl({$paletteConfig.mainColor.hue -
+            30}, 100%, 65%) 0%, hsl({$paletteConfig.mainColor.hue + 30}, 100%, 65%) 100%);"
         type="range"
         min="-100"
         max="100"
-        class="range range-sm"
         on:input={setHueRotationAmount}
         value={$paletteConfig.hueRotationAmount}
     />
 </div>
+
+<style>
+    /* 
+    Custom slider styling
+    Credit: https://www.w3schools.com/howto/howto_js_rangeslider.asp 
+    */
+
+    /* The slider itself */
+    .slider {
+        -webkit-appearance: none; /* Override default CSS styles */
+        appearance: none;
+    }
+
+    /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+    .slider::-webkit-slider-thumb {
+        -webkit-appearance: none; /* Override default look */
+        appearance: none;
+        width: 25px; /* Set a specific slider handle width */
+        height: 25px; /* Slider handle height */
+        background: #fff; /* Green background */
+        cursor: pointer; /* Cursor on hover */
+        border-radius: 100%;
+        border: 2px solid #90949e;
+        transition: transform 0.2s ease-in-out;
+    }
+
+    .slider::-webkit-slider-thumb:hover {
+        transform: scale(1.1);
+    }
+
+    .slider::-moz-range-thumb {
+        width: 25px; /* Set a specific slider handle width */
+        height: 25px; /* Slider handle height */
+        background: #fff; /* Green background */
+        cursor: pointer; /* Cursor on hover */
+        border-radius: 100%;
+        border: 2px solid #90949e;
+        transition: transform 0.2s ease-in-out;
+    }
+
+    .slider::-moz-range-thumb:hover {
+        transform: scale(1.1);
+    }
+</style>
