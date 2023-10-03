c26934622ee34c24c2a2a7ae4b2b82609ca89184
Jesus-Cries <16zeichen@gmail.com>
2023-10-03 17:52:52 +0200

    Create own component for custom slider

--- src/routes/projects/paletti/HueSlider.svelte
+++ src/routes/projects/paletti/HueSlider.svelte
@@ -2,7 +2,7 @@
     import convert from "color-convert"
     import { page } from "$app/stores"
     import { getContext } from "svelte"
-    import SettingWrapper from "./SettingWrapper.svelte"
+    import Slider from "./Slider.svelte"
 
     const navigate: (mainColor?: string, hueRotationAmount?: number) => void =
         getContext("navigate")
@@ -18,60 +18,12 @@
     $: mainHue = convert.hex.hsl($page.data.mainColor)[0]
 </script>
 
-<SettingWrapper label="Hue">
-    <input
-        class="slider h-3 w-full rounded-full"
-        style="background: linear-gradient(90deg, hsl({mainHue - 30}, 100%, 65%) 0%, hsl({mainHue +
-            30}, 100%, 65%) 100%);"
-        type="range"
-        min="-100"
-        max="100"
-        step="10"
-        on:input={setHueRotationAmount}
-        value={$page.data.hueRotationAmount}
-    />
-</SettingWrapper>
-
-<style>
-    /* 
-    Custom slider styling
-    Credit: https://www.w3schools.com/howto/howto_js_rangeslider.asp 
-    */
-
-    /* The slider itself */
-    .slider {
-        -webkit-appearance: none; /* Override default CSS styles */
-        appearance: none;
-    }
-
-    /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
-    .slider::-webkit-slider-thumb {
-        -webkit-appearance: none; /* Override default look */
-        appearance: none;
-        width: 25px; /* Set a specific slider handle width */
-        height: 25px; /* Slider handle height */
-        background: #fff; /* Green background */
-        cursor: pointer; /* Cursor on hover */
-        border-radius: 100%;
-        border: 2px solid #90949e;
-        transition: transform 0.2s ease-in-out;
-    }
-
-    .slider::-webkit-slider-thumb:hover {
-        transform: scale(1.1);
-    }
-
-    .slider::-moz-range-thumb {
-        width: 25px; /* Set a specific slider handle width */
-        height: 25px; /* Slider handle height */
-        background: #fff; /* Green background */
-        cursor: pointer; /* Cursor on hover */
-        border-radius: 100%;
-        border: 2px solid #90949e;
-        transition: transform 0.2s ease-in-out;
-    }
-
-    .slider::-moz-range-thumb:hover {
-        transform: scale(1.1);
-    }
-</style>
+<Slider
+    style="background: linear-gradient(90deg, hsl({mainHue - 30}, 100%, 65%) 0%, hsl({mainHue +
+        30}, 100%, 65%) 100%);"
+    min={-100}
+    max={100}
+    step={10}
+    value={$page.data.hueRotationAmount}
+    onInput={setHueRotationAmount}
+/>
