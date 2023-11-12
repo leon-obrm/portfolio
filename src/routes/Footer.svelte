4f9d11d7d43df05376fb33c9b2421cb47abfc4d8
Jesus-Cries <16zeichen@gmail.com>
2023-11-12 22:55:00 +0100

    Mirror footer waves to improve mobile look

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,7 +1,7 @@
 <div class="relative h-64 overflow-hidden">
     <div
-        class="absolute bottom-0 h-[1080px] w-screen"
-        style="background-image: url('/FooterWaves.svg'); background-size: cover;"
+        class="absolute bottom-0 h-[1080px] w-screen bg-cover"
+        style="background-image: url('/FooterWaves.svg');"
     />
     <div class="absolute bottom-6 w-screen text-center text-sm text-white">
         Copyright © 2023 Leon Obermann
