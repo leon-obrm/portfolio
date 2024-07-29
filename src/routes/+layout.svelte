55de30d2c16962050adf4317e17d47a54273f40e
Jesus-Cries <16zeichen@gmail.com>
2024-07-29 22:43:27 +0200

    Wipe everything for reinstallation

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import "../app.css"
-</script>
-
-<div
-    class="selection:bg-gray-700 selection:text-white selection:dark:bg-gray-50 selection:dark:text-black"
->
-    <slot />
-</div>
