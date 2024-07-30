148702619a325264d86d436982a0c464c1e51ada
Jesus-Cries <16zeichen@gmail.com>
2024-07-30 23:51:03 +0200

    Add image glow to tailwind config

--- src/routes/homepage/ContactPortrait.svelte
+++ src/routes/homepage/ContactPortrait.svelte
@@ -1,3 +1,7 @@
 <div class="flex w-full justify-center pb-8 pt-12">
-    <img class="max-w-[65%] rounded-[65px]" src="Rooftop-min.JPG" alt="Portrait of Leon Obermann" />
+    <img
+        class="max-w-[65%] rounded-[65px] shadow-image-glow"
+        src="Rooftop-min.JPG"
+        alt="Portrait of Leon Obermann"
+    />
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
                 nunito: ["Nunito", ...defaultTheme.fontFamily.sans],
                 "inria-sans": ["Inria Sans", ...defaultTheme.fontFamily.sans],
             },
+            boxShadow: {
+                "image-glow": "0px 0px 105px rgba(255, 255, 255, 0.1)",
+            },
         },
     },
     plugins: [],
