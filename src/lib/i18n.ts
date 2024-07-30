2e1ab1c1d2e65cbdeef50c69fa1134c542f1a875
Jesus-Cries <16zeichen@gmail.com>
2024-07-30 20:26:42 +0200

    Add basic i18n

---
+++ src/lib/i18n.ts
@@ -0,0 +1,24 @@
+import i18next from "i18next"
+import { createI18nStore } from "svelte-i18next"
+
+i18next.init({
+    lng: "en",
+    resources: {
+        en: {
+            translation: {
+                key: "hello world",
+            },
+        },
+        de: {
+            translation: {
+                key: "Hallo Welt",
+            },
+        },
+    },
+    interpolation: {
+        escapeValue: false, // not needed for svelte as it escapes by default
+    },
+})
+
+const i18n = createI18nStore(i18next)
+export default i18n
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import i18n from "$lib/i18n"
+</script>
+
+<h1>{$i18n.t("key")}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
