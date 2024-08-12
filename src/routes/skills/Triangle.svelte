f69f3d1d3b638a6cae9744e2e10f4494246bfbfe
Jesus-Cries <16zeichen@gmail.com>
2024-08-12 11:50:58 +0200

    Replace CSS triangles with SVGs

--- src/routes/skills/Triangle.svelte
+++ src/routes/skills/Triangle.svelte
@@ -1,30 +1,7 @@
+<script lang="ts">
+    import { Triangle } from "lucide-svelte"
+</script>
+
 <div class="rotate-180 lg:scale-[2]">
-    <div class="triangle relative bg-white"></div>
+    <Triangle fill="white" size={12} />
 </div>
-
-<style>
-    /* Credit: https://stackoverflow.com/a/14451916 */
-    .triangle:before,
-    .triangle:after {
-        content: "";
-        position: absolute;
-        background-color: inherit;
-    }
-    .triangle,
-    .triangle:before,
-    .triangle:after {
-        width: 7px;
-        height: 7px;
-        border-top-right-radius: 40%;
-    }
-
-    .triangle {
-        transform: rotate(-60deg) skewX(-30deg) scale(1, 0.866);
-    }
-    .triangle:before {
-        transform: rotate(-135deg) skewX(-45deg) scale(1.414, 0.707) translate(0, -50%);
-    }
-    .triangle:after {
-        transform: rotate(135deg) skewY(-45deg) scale(0.707, 1.414) translate(50%);
-    }
-</style>
