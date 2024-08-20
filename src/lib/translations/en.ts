684e452e4f1c67d09fb5dd700c93ad9d5576a305
Leon Obermann <67223471+Jesus-Cries@users.noreply.github.com>
2024-08-21 01:13:52 +0200

    Fix spelling mistake

--- src/lib/translations/en.ts
+++ src/lib/translations/en.ts
@@ -18,7 +18,7 @@ export const en = {
         // Projects
         palettiTimeframe: "August 2023 - Now",
         palettiParagraphs:
-            "Choosing the perfect colors for web designs is hard. And even if you know how to do it right, it still takes a lot of time. That's why I created Paletti.\nPaletti is a user-friendly tool for quickly generating indivdual color palettes for web design and development.",
+            "Choosing the perfect colors for web designs is hard. And even if you know how to do it right, it still takes a lot of time. That's why I created Paletti.\nPaletti is a user-friendly tool for quickly generating individual color palettes for web design and development.",
         boredomBusterTimeframe: "August 2023",
         boredomBusterParagraphs:
             "Finding something fun to do can be challenging. This is where the Boredom Buster comes in.\nJust hit the button, and it'll randomly suggest an exciting activity and the perfect amount of time to get up and enjoy yourself.",
