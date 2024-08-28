f3ea80173ba4a39b7331e7893ee064d282f9d3b5
Jesus-Cries <16zeichen@gmail.com>
2024-08-28 13:50:36 +0200

    Address target group directly in paletti description

--- src/lib/translations/de.ts
+++ src/lib/translations/de.ts
@@ -20,7 +20,7 @@ export const de: ITranslation = {
         // Projects
         palettiTimeframe: "August 2023 - Jetzt",
         palettiParagraphs:
-            "Die richtigen Farben für ein Web Design zu finden ist schwierig. Selbst wenn man weiß, wie man es richtig anstellt, nimmt es viel Zeit in Anspruch. Deshalb habe ich Paletti entwickelt.\nPaletti ist ein benutzerfreundliches Tool zur schnellen Erstellung individueller Farbpaletten für Webdesign und Webentwicklung.",
+            "Die richtigen Farben für ein Web Design zu finden ist schwierig. Selbst wenn man weiß, wie man es richtig anstellt, nimmt es viel Zeit in Anspruch. Deshalb habe ich Paletti entwickelt.\nPaletti ist ein benutzerfreundliches Tool zur schnellen Erstellung individueller Farbpaletten für Webdesigner und Webentwickler.",
         boredomBusterTimeframe: "August 2023",
         boredomBusterParagraphs:
             "Es ist nicht immer einfach, eine spannende Aktivität zu finden. Genau hier kommt der Boredom Buster ins Spiel.\nEinfach den Button drücken und er schlägt dir zufällig eine unterhaltsame Aktivität und die ideale Dauer vor, um aufzustehen und Spaß zu haben.",
