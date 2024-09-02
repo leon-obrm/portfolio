70e3d3266a289c2da84134a0da47713b2fd7ccf8
Jesus-Cries <16zeichen@gmail.com>
2024-09-02 13:25:02 +0200

    Move website wrapper to higher component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,8 @@
 
     import getI18nStore from "$lib/i18n"
     import { setContext } from "svelte"
+    import NavBar from "./navbar/NavBar.svelte"
+    import Footer from "./footer/Footer.svelte"
 
     // Adapts i18n to usage with SvelteKit (Avoids unwanted syncs between different frontends)
     setContext("i18n", getI18nStore())
@@ -17,7 +19,15 @@
     // ======== Bug Fixes ========
     // FIXME: Skill sections lags (Other sections too, but this one is the most noticeable)
     // FIXME:? Zooming on laptop touchpad creates white vignette
+
+    import { useI18n } from "$lib/useI18n"
+
+    const i18n = useI18n()
 </script>
+
+<svelte:head>
+    <title>{$i18n.t("leonObermannsPortfolio")}</title>
+</svelte:head>
 
 <ScrollUpButton />
 
@@ -26,5 +36,11 @@
 <MouseFollow />
 
 <div class="relative flex w-full flex-col overflow-clip font-nunito text-white">
-    <slot />
+    <NavBar />
+
+    <div class="w-full p-5 pb-16 lg:p-10 lg:pb-28">
+        <slot />
+    </div>
+
+    <Footer />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,13 @@
 <script lang="ts">
-    import NavBar from "./navbar/NavBar.svelte"
     import Homepage from "./homepage/Homepage.svelte"
     import Projects from "./projects/Projects.svelte"
     import Skills from "./skills/Skills.svelte"
     import About from "./about/About.svelte"
     import Contact from "./contact/Contact.svelte"
-    import Footer from "./footer/Footer.svelte"
-
-    import { useI18n } from "$lib/useI18n"
-
-    const i18n = useI18n()
 </script>
 
-<svelte:head>
-    <title>{$i18n.t("leonObermannsPortfolio")}</title>
-</svelte:head>
-
-<NavBar />
-
-<div class="w-full p-5 pb-16 lg:p-10 lg:pb-28">
-    <Homepage />
-    <Projects />
-    <Skills />
-    <About />
-    <Contact />
-</div>
-
-<Footer />
+<Homepage />
+<Projects />
+<Skills />
+<About />
+<Contact />
