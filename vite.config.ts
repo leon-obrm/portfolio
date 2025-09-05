549a43560672492d7b6f99da8da4116135595216
Jesus-Cries <16zeichen@gmail.com>
2025-09-06 00:25:38 +0200

    Fix long loading time due to lucide

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
+// Makes sure that lucide-svelte can be used with automatic imports and still have fast reload times
+// Auto-updates if lucide is updated (May take a few hours)
+import lucidePreprocess from "vite-plugin-lucide-preprocess"
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [lucidePreprocess(), sveltekit()],
 })
