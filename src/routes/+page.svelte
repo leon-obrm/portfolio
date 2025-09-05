e5b7ad133db91dd6c1ebdb19ff229cfb6274d4b4
Jesus-Cries <16zeichen@gmail.com>
2025-09-06 00:33:33 +0200

    Scroll to scroll destination on page load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,30 @@
     import Skills from "./skills/Skills.svelte"
     import About from "./about/About.svelte"
     import Contact from "./contact/Contact.svelte"
+    import { onMount } from "svelte"
+
+    // Scrolls to scroll destination on page load
+    onMount(() => {
+        /** ID of scroll destination. */
+        const hash: string = window.location.hash
+
+        // Do nothing if there is no scroll destination
+        if (!hash) return
+
+        // Delay slightly to ensure DOM is painted
+        requestAnimationFrame(() => {
+            /** Element to scroll to. */
+            const scrollDestination = document.querySelector(hash)
+
+            // Do nothing if there is no element to scroll to
+            if (!scrollDestination) return
+
+            // Scroll to scroll destination
+            setTimeout(() => {
+                scrollDestination.scrollIntoView({ behavior: "auto" })
+            }, 250)
+        })
+    })
 </script>
 
 <Homepage />
